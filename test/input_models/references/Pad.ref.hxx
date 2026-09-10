e4ae3be3b9abb2b960c2bfb95f00e334ceda5681
Harsh Chauhan <harsh2005.hc@gmail.com>
2026-09-11 03:22:26 +0530

    feat: launch kernel as a task in PAD operator

--- test/input_models/references/Pad.ref.hxx
+++ test/input_models/references/Pad.ref.hxx
@@ -1,5 +1,3 @@
-// Reference output for Pad.onnx ([1,2,2] -> [2,3,5], constant mode, pads before=[1,0,1] after=[0,1,2])
-// Expected computed with numpy.pad
 #pragma once
 namespace Pad_ExpectedOutput {
    static float outputs[30] = {
